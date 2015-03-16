c0a26c0d4905d300ea6edcd7925116dbafd6308e
mobeets <mobeets@gmail.com>
2015-03-16 14:26:25 -0500

    sepVsScore shows cell types in plot; fixes #6

--- +plot/separabilityVsScore.m
+++ +plot/separabilityVsScore.m
@@ -6,6 +6,20 @@ function fig = separabilityVsScore(fitdir)
         val = io.summaryByDate(dts{ii}, fitdir, foldind);
         vals = [vals val];
     end
-    fig = figure; plot([vals.separability], [vals.score], 'ok');
-    xlabel('separability index'); ylabel('fit score');
+    
+    fig = figure; hold on;
+    cellTypes = unique({vals.cellType});
+    clrs = lines(numel(cellTypes)+1);
+    for ii = 1:numel(cellTypes)
+        lbl = cellTypes(ii);
+        inds = strcmp({vals.cellType}, lbl);
+        plot([vals(inds).separability], [vals(inds).score], 'o', ...
+            'Color', clrs(ii,:), 'MarkerFaceColor', clrs(ii,:), ...
+            'DisplayName', lbl);
+    end
+    plot([min([vals.separability]) max([vals.separability])], [0 0], ...
+        '--', 'Color', 'k', 'HandleVisibility', 'off');
+    legend('Location', 'NorthEastOutside');
+    xlabel('separability index');
+    ylabel('fit score');
 end
