2c60a9ad464d3d2ac356ad5cb46ba66909c5abf6
Jacob Yates <jacobyates@LA-CPS828325MN-Huk.austin.rr.com>
2015-07-26 11:41:01 -0500

    figure 2 cleaned up a little closer to a print version

--- +figure/figure2.m
+++ +figure/figure2.m
@@ -1,53 +1,57 @@
 %% Single Cell Prediction Comparison
 
-exampleCellMT = '20140305-MT_2';
+exampleCellMT = '20140304-MT_12';
 vMT = vuMT(strcmp({vuMT.name}, exampleCellMT));
 d = io.loadDataByDate(vMT.dt, vMT.isNancy);
 goodCells = find([vuMT.dPrime] > 1);
 
-h = plot.trueVsModelSpikes(vMT);
+idx = 300:400;
+h = plot.trueVsModelSpikes(vMT, idx);
 
+% sample trials model fit quality
 figure(h(1))
-idx = 300+[0 100];
-xlim(idx)
-set(h(1), 'PaperSize', [4 1], 'PaperPosition', [0 0 4 1])
-title(strrep(vMT.name, '_', ' '))
-saveas(h(1), fullfile(figDir, sprintf('trialCompare%d.pdf', exampleCellMT)))
+annotation('textbox', [.1 .8 .2 .05], 'String', strrep(vMT.name, '_', ' '), 'Linestyle', 'None')
+title('')
+figure.cleanupForPrint(h(1), 'FontSize', 8, 'PaperSize', [80 25])
+saveas(h(1), fullfile(figDir, sprintf('trialCompare%d.png', exampleCellMT)))
 
+% for saving out vector graphics with patch objects
+% figure.cleanupForPrint(h(1), 'FontSize', 6, 'PaperSize', [80 25], true)
+% plot2svg('~/Desktop/test2.svg', h(1))
+
+% Spike Rate as a function of Net Motion
 figure(h(3))
-set(h(3), 'PaperSize', [4 4], 'PaperPosition', [0 0 4 4])
+figure.cleanupForPrint(h(3), 'FontSize', 8, 'PaperSize', [25 25])
 saveas(h(3), fullfile(figDir, sprintf('MotionDirSensitivity%s.pdf', exampleCellMT)))
 
-%%
+%% Space-time image of a single trial
 figure(110); clf
-% tr = find(d.stim.dirprob==0,1);
 tr = randi(d.stim.nTrials);
-imagesc(squeeze(d.stim.pulses(tr,:,:))'); %colormap gray
+imagesc(squeeze(d.stim.pulses(tr,:,:))', [-1 1]); colormap gray
+set(gca, 'Xtick', [1 d.stim.nPulses], 'Ytick', [1 d.stim.nGabors])
 xlabel('Pulse #')
 ylabel('Gabor Id')
-set(gcf, 'PaperSize', [4 4], 'PaperPosition', [0 0 4 4])
-title(strrep(vMT.name, '_', ' '))
-% saveas(gcf, fullfile(figDir, sprintf('stimTrial%d.pdf', kNeuron)))
+figure.cleanupForPrint(gcf, 'FontSize', 8, 'PaperSize', [25 25])
+plot.saveFigure(sprintf('stimTrial%d.png', tr), figDir, gcf);
 
-%%
+
+%% All the trials
 figure(111); clf
-imagesc(reshape(d.stim.pulses, d.stim.nTrials, [])'); colormap gray
-xlim(idx)
-set(gcf, 'PaperSize', [4 1], 'PaperPosition', [0 0 4 1])
-title(strrep(vMT.name, '_', ' '))
-saveas(gcf, fullfile(figDir, sprintf('stimulus%d.pdf', exampleCellMT)))
+imagesc(reshape(d.stim.pulses(idx,:,:), numel(idx), [])'); colormap gray
+set(gca, 'XTick', [1 numel(idx)], 'Ytick', [1 d.stim.nGabors*d.stim.nPulses])
+xlabel('Trial')
+ylabel('S(Trial)')
+figure.cleanupForPrint(gcf, 'FontSize', 8, 'PaperSize', [80 25])
+plot.saveFigure(sprintf('stimulus%d.png', exampleCellMT), figDir, gcf)
+
 
 %% r-sq before and after AR-2 model
-
-figure;
-set(gca, 'FontSize', 5);
+figure(112); clf
 set(gcf, 'color', 'w');
 plot.fitSummaries(vuMT, 'b', 'score_AR', nan, false);
 xlim([0 100])
 hold on
-plot([5 5], ylim, 'k')
-% ylim([0 1])
 xlabel('Mean Spike Rate')
 ylabel('r^2')
-set(gcf, 'PaperSize', [4 4], 'PaperPosition', [0 0 4 4])
-saveas(gcf, fullfile(figDir, 'rsquared.pdf'))
+figure.cleanupForPrint(gcf, 'FontSize', 8, 'PaperSize', [25 25])
+plot.saveFigure('modelPerformancePopulation', figDir, gcf)
--- +plot/trueVsModelSpikes.m
+++ +plot/trueVsModelSpikes.m
@@ -1,28 +1,44 @@
-function h = trueVsModelSpikes(v)
+function h = trueVsModelSpikes(v, idx)
+    if ~exist('idx', 'var')
+        idx = 1:100;
+    end
     Y = v.Y;
     Yh = v.Yh;
     Yh1 = v.YhAR;
     ix = ~isnan(Y); Y = Y(ix); Yh = Yh(ix); Yh1 = Yh1(ix);
     
     Fs = 5;
-    h(1) = figure; hold on; set(gcf, 'color', 'w'); set(gca, 'FontSize', Fs);
+    h(1) = figure;
+    ax = tight_subplot(1,1,.1, .1, .1);
+    axes(ax)
+    
+    Yp = Y(idx);
+    Yhp = Yh1(idx);
+    xx = [1 1:numel(Yp) numel(Yp)];
+    yy = [0 Yp' 0];
+    fill(xx(:),yy(:),'k', 'FaceColor', .5*[1 1 1], 'Linestyle', 'none'); hold on
+    plot(Yhp, 'r', 'Linewidth', 1);   
+    set(gcf, 'color', 'w');
     xlabel('trial'); ylabel('spike count'); title(v.name);
-    xx = [1 1:numel(Y) numel(Y)];
-    yy = [0 Y' 0];
-    plot(xx,yy,'k');
-%     fill(xx,yy,'k', 'FaceColor', .5*[1 1 1])
-%     stairs(Y, 'k');
-%     stairs(Yh, 'b');
-    plot(Yh1, 'r', 'Linewidth', 1);    
 
-    h(2) = figure; hold on; set(gcf, 'color', 'w'); set(gca, 'FontSize', Fs);
+        
+    xlim([0 numel(Yp)])
+    h(2) = figure;
+    ax = tight_subplot(1,1,.1, .1, .1);
+	set(gcf, 'color', 'w'); 
+    axes(ax)
+    set(gca, 'FontSize', Fs);
     xlabel('dirprob'); ylabel('spike count'); title(v.name);
     xs = v.dirprob(ix);
-    scatter(xs, Y, 'b');
+    scatter(xs, Y, 'b');hold on;
     scatter(xs, Yh1, 'r');
     
 
-    h(3) = figure; hold on; set(gcf, 'color', 'w'); set(gca, 'FontSize', Fs);
+    h(3) = figure; 
+    ax = tight_subplot(1,1,1, .1, .1);
+    hold on; set(gcf, 'color', 'w');
+    axes(ax)
+    set(gca, 'FontSize', Fs);
     xlabel('dirstrength'); ylabel('spike count'); title(v.name);
     xs = v.dirstrength(ix);  
     [xso, ix] = sort(xs);
