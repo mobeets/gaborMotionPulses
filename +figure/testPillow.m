308fa75bcaef88ec701b4e5acb87b80bf1c15d84
mobeets <mobeets@gmail.com>
2019-01-31 13:56:51 -0500

    check

--- +figure/testPillow.m
+++ +figure/testPillow.m
@@ -13,7 +13,6 @@ dts = unique({pairs.dt});
 % of units
 % 
 
-plot.init;
 pts = [];
 for jj = 1:numel(dts)
     dtstr = dts{jj};
@@ -25,25 +24,31 @@ for jj = 1:numel(dts)
         cell1 = cells(ismember({cells.name}, cpair.cell1));
         cell2 = cells(ismember({cells.name}, cpair.cell2));
         rf1 = cell1.w; rf2 = cell2.w;
-        cpts(ii,1) = rf1'*rf2;
+        cpts(ii,1) = rf1'*rf2;        
 
         noiseCov = nancov(cpair.Ys_resAR);
         cpts(ii,2) = noiseCov(1,2);
+        
+%         cpts(ii,2) = cpts(ii,1);
+%         cv = cov(rf1, rf2); cpts(ii,1) = cv(1,2);
+%         cpts(ii,1) = cpair.rfCorr;
+%         cpts(ii,1) = cpair.noiseCorrAR;
         
         ixd = ~any(isnan(cpair.Ys_resAR),2);
         assert(corr(cpair.Ys_resAR(ixd,1), cpair.Ys_resAR(ixd,2)) == ...
             cpair.noiseCorrAR);
     end
-    pts = [pts; cpts];
-    plot(cpts(:,1), cpts(:,2), 'k.', 'MarkerSize', 20);
+    pts = [pts; cpts];    
 end
+plot.init;
+plot(pts(:,1), pts(:,2), 'k.', 'MarkerSize', 20);
 xlabel('predicted noise cov. (using STRFs)');
 ylabel('actual noise cov. (using residuals)');
 title(['r^2 = ' sprintf('%0.2f', corr(pts(:,1), pts(:,2)))]);
 
 %% part 2. noise correlation predicts delta decoding (with groups of cells)
 
-ks = [2]; % 2 = pairs, 3 = triplets, 4 = quads, etc. (can include multiple)
+ks = 2:4; % 2 = pairs, 3 = triplets, 4 = quads, etc. (can include multiple)
 fitToShuffled = true; % if false, fit to unshuffled data
 
 ixCellsToKeep = figure.filterCellsAndPairs(cells, false);
@@ -57,20 +62,18 @@ if fitToShuffled
 else
     shufnm = 'unshuffled';
 end
-cnm = ['weights fit to ' shufnm '; k= ' num2str(ks)];
+cnm = ['weights fit to ' shufnm];% '; k= ' num2str(ks)];
 % todo next:
 % - ideally I think we'd like the prediction to be in terms of
 % delta-percent correct, since that's what we use elsewhere in the paper
-% - compare to fitting weights to shuffled OR unshuffled data
 % - need to do cross-val on model fitting
 
-plot.init;
 pts = [];
 for jj = 1:numel(dts)
     dtstr = dts{jj};
     ix = ismember({groups.dt}, dtstr);
     cgroups = groups(ix);
-    cpts = nan(numel(cgroups), 2);
+    cpts = nan(numel(cgroups), 3);
     for ii = 1:numel(cgroups)
         cgroup = cgroups(ii);
         Ys_unshuf = cgroup.Ys_resAR;
@@ -113,9 +116,22 @@ for jj = 1:numel(dts)
         dPrime_shuf = tools.dprime(Ys_shuf*L + K, cgroup.stimdir);
         dPrime_raw = tools.dprime(Ys_unshuf*L + K, cgroup.stimdir);
         cpts(ii,2) = sqrt(dPrime_raw/dPrime_shuf);
+        cpts(ii,3) = numel(ws); % whether pair, triplet, etc.
     end
     pts = [pts; cpts];
-    plot(cpts(:,1), cpts(:,2), 'k.', 'MarkerSize', 20);
+end
+
+%%
+
+plot.init;
+% plot(pts(:,1).^2, pts(:,2).^2, 'k.', 'MarkerSize', 20);
+nks = unique(pts(:,3));
+clrs = [0.8 0.2 0.2; 0.2 0.2 0.8; 0.2 0.8 0.2];
+for kk = numel(nks):-1:1
+    ixc = pts(:,3) == nks(kk);
+    clr = clrs(kk,:);
+    plot(pts(ixc,1).^1, pts(ixc,2).^1, '.', 'Color', clr, ...
+        'MarkerSize', 1);
 end
 xlabel({'predicted d-prime ratio', ...
     '\leftarrow r_{sc} hurts         r_{sc} helps \rightarrow'});
@@ -123,6 +139,6 @@ ylabel({'actual d-prime ratio', ...
     '\leftarrow r_{sc} hurts         r_{sc} helps \rightarrow'});
 ixc = ~any(isnan(pts(:,1:2)),2);
 axis equal;
-xlim([0.5 1.5]); ylim(xlim);
+xlim([0.5 1.6]); ylim(xlim);
 set(gca, 'XTick', [0.5 1 1.5]); set(gca, 'YTick', [0.5 1 1.5]);
 title({cnm, ['r^2 = ' sprintf('%0.2f', corr(pts(ixc,1), pts(ixc,2)))]});
